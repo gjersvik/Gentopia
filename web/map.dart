b28c001b98e3d39a79a84995003557ba89304404
Ole Martin Gjersvik <olem@olem.org>
2013-09-01 16:05:11 +0200

    No show 4 tiles

--- web/map.dart
+++ web/map.dart
@@ -15,9 +15,30 @@ class Maper{
     
   }
   
-  start([_]) {
+  start() {
+    load.getTile(-16, -16, 16, 16).then((data){
+      ImageElement img = new ImageElement(src:data);
+      elem.append(img);
+    });
+
+    load.getTile(0, -16, 16, 16).then((data){
+      ImageElement img = new ImageElement(src:data);
+      img.style.left = '512px';
+      elem.append(img);
+    });
+    
+
+    load.getTile(-16, 0, 16, 16).then((data){
+      ImageElement img = new ImageElement(src:data);
+      img.style.top = '512px';
+      elem.append(img);
+    });
+    
+
     load.getTile(0, 0, 16, 16).then((data){
       ImageElement img = new ImageElement(src:data);
+      img.style.left = '512px';
+      img.style.top = '512px';
       elem.append(img);
     });
   }
