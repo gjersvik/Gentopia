f3a6f4731c70eea7e512268bb8fcc56d84b0167b
Ole Martin Gjersvik <olem@olem.org>
2013-09-01 21:37:07 +0200

    Puts map round center.

--- web/map.dart
+++ web/map.dart
@@ -6,40 +6,54 @@ import 'dart:isolate';
 
 part 'src/map/worldloader.dart';
 
+class Tile{
+  final int x;
+  final int y;
+  final ImageElement img;
+  
+  Tile(this.x, this.y, this.img);
+  put(int x,int y){
+    img.style.left = '${x}px';
+    img.style.top = '${y}px';
+  }
+}
+
 class Maper{
   Element elem;
   
   WorldLoader load = new WorldLoader();
+  List<Tile> list = [];
   
   Maper(this.elem){
     
   }
   
   start() {
-    load.getTile(-1, -1, 1, 1).then((data){
-      ImageElement img = new ImageElement(src:data);
-      elem.append(img);
-    });
-
-    load.getTile(0, -1, 1, 1).then((data){
-      ImageElement img = new ImageElement(src:data);
-      img.style.left = '512px';
-      elem.append(img);
-    });
+    list.add(getTile(-1,-1));
+    list.add(getTile(-0,-1));
+    list.add(getTile(-1,0));
+    list.add(getTile(0,0));
     
-
-    load.getTile(-1, 0, 1, 1).then((data){
-      ImageElement img = new ImageElement(src:data);
-      img.style.top = '512px';
-      elem.append(img);
+    updatePos();
+  }
+  
+  Tile getTile(int x, int y){
+    Tile tile = new Tile(x,y,new ImageElement());
+    elem.append(tile.img);
+    load.getTile(x, y, 1, 1).then((data){
+      tile.img.src = data;
     });
+    return tile;
+  }
+  
+  updatePos(){
+    num x = 0 + elem.borderEdge.width / 2;
+    num y = 0 + elem.borderEdge.height / 2;
     
-
-    load.getTile(0, 0, 1, 1).then((data){
-      ImageElement img = new ImageElement(src:data);
-      img.style.left = '512px';
-      img.style.top = '512px';
-      elem.append(img);
+    list.forEach((t){
+      int lx = (x + t.x * 512).toInt();
+      int ly = (y + t.y * 512).toInt();
+      t.put(lx, ly);
     });
   }
   
