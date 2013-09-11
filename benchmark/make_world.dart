6f101b806c7f95fc03c3599b36c43d83e9781e6e
Ole Martin Gjersvik <olem@olem.org>
2013-09-11 18:27:47 +0200

    Made a hash benkmark

--- benchmark/make_world.dart
+++ benchmark/make_world.dart
@@ -1,9 +1,21 @@
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:gentopia/gentopia.dart';
 
-class MakeWorldBenchmark extends BenchmarkBase {
+class HashBenchmark extends BenchmarkBase {
+  Hash h = new Hash('Ole Martin');
+  HashBenchmark() : super("Hash");
+
+  // The benchmark code.
+  void run() {
+    h.hash('321x132l5');
+  }
+}
+
+
+
+class WorldBenchmark extends BenchmarkBase {
   World w = new World();
-  MakeWorldBenchmark() : super("Make World");
+  WorldBenchmark() : super("World");
 
   // The benchmark code.
   void run() {
@@ -12,6 +24,7 @@ class MakeWorldBenchmark extends BenchmarkBase {
 }
 
 main() {
-  new MakeWorldBenchmark().report();
+  new HashBenchmark().report();
+  new WorldBenchmark().report();
 }
 
